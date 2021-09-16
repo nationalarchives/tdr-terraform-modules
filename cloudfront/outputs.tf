b37d011cb009dc5d68bc64c671328dc9f054f66f
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-16 12:09:08 +0100

    Add domain name and hosted zone to outputs

--- cloudfront/outputs.tf
+++ cloudfront/outputs.tf
@@ -1,3 +1,11 @@
 output "cloudfront_oai_iam_arn" {
   value = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
 }
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.cloudfront_s3_distribution.domain_name
+}
+
+output "cloudfront_hosted_zone_id" {
+  value = aws_cloudfront_distribution.cloudfront_s3_distribution.hosted_zone_id
+}
