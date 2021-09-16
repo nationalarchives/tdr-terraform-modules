fecc5145a3241cd54588bd10a7a85a985a30819c
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-16 11:33:09 +0100

    Add cloudfront distribution.
    This has the origins, behaviour and logging set up. It does not have
    custom domain names or user restrictions based on cookies set up yet.
    These will be added later.

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -1,2 +1,64 @@
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
 }
+
+resource "aws_cloudfront_origin_request_policy" "request_policy" {
+  name = "s3-multipart-upload"
+  headers_config {
+    header_behavior = "whitelist"
+    headers {
+      items = ["Origin", "access-control-request-headers", "access-control-request-method"]
+    }
+  }
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+  cookies_config {
+    cookie_behavior = "none"
+  }
+}
+
+data "aws_cloudfront_cache_policy" "caching_disabled" {
+  name = "Managed-CachingDisabled"
+}
+
+resource "aws_cloudfront_distribution" "cloudfront_s3_distribution" {
+  enabled = true
+  logging_config {
+    bucket = var.logging_bucket_regional_domain_name
+  }
+  default_cache_behavior {
+    allowed_methods = [
+      "HEAD",
+      "DELETE",
+      "POST",
+      "GET",
+      "OPTIONS",
+      "PUT",
+      "PATCH"
+    ]
+    cached_methods = [
+      "HEAD",
+      "GET"
+    ]
+    target_origin_id         = local.origin_id
+    viewer_protocol_policy   = "https-only"
+    cache_policy_id          = data.aws_cloudfront_cache_policy.caching_disabled.id
+    origin_request_policy_id = aws_cloudfront_origin_request_policy.request_policy.id
+
+  }
+  origin {
+    domain_name = var.s3_regional_domain_name
+    origin_id   = local.origin_id
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
