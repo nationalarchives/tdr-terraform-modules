a82c6afb51097eba6bfedac1099f0344817de6b4
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-02-09 09:19:54 +0000

    Add modules for github and identity provider

---
+++ identity_provider/variables.tf
@@ -0,0 +1,13 @@
+variable "thumbprint" {
+  description = "The thumbprint for the OIDC provider. These are public"
+}
+
+variable "audience" {
+  description = "The audience for the OIDC provider"
+}
+
+variable "url" {
+  description = "The url for the OIDC provider"
+}
+
+variable "common_tags" {}
