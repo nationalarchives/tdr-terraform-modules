54d889441321ab87133182f69a16fe2fd77cd1d9
TomJKing <tom.king@nationalarchives.gov.uk>
2022-02-16 08:55:45 +0000

    Name key correctly

--- lambda/notifications.tf
+++ lambda/notifications.tf
@@ -14,6 +14,7 @@ resource "aws_lambda_function" "notifications_lambda_function" {
       SLACK_WEBHOOK         = aws_kms_ciphertext.environment_vars_notifications["slack_webhook"].ciphertext_blob
       TO_EMAIL              = aws_kms_ciphertext.environment_vars_notifications["to_email"].ciphertext_blob
       MUTED_VULNERABILITIES = aws_kms_ciphertext.environment_vars_notifications["muted_vulnerabilities"].ciphertext_blob
+      TRANSFORM_ENGINE_SQS  = aws_kms_ciphertext.environment_vars_notifications["transform_engine_sqs"].ciphertext_blob
     }
   }
 
@@ -23,7 +24,7 @@ resource "aws_lambda_function" "notifications_lambda_function" {
 }
 
 resource "aws_kms_ciphertext" "environment_vars_notifications" {
-  for_each = local.count_notifications == 0 ? {} : { slack_webhook = data.aws_ssm_parameter.slack_webhook[0].value, to_email = "tdr-secops@nationalarchives.gov.uk", muted_vulnerabilities = join(",", var.muted_scan_alerts), transfer_engine_sqs = data.aws_ssm_parameter.transform_engine_sqs[0].value }
+  for_each = local.count_notifications == 0 ? {} : { slack_webhook = data.aws_ssm_parameter.slack_webhook[0].value, to_email = "tdr-secops@nationalarchives.gov.uk", muted_vulnerabilities = join(",", var.muted_scan_alerts), transform_engine_sqs = data.aws_ssm_parameter.transform_engine_sqs[0].value }
   # This lambda is created by the tdr-terraform-backend project as it only exists in the management account so we can't use any KMS keys
   # created by the terraform environments project as they won't exist when we first run the backend project.
   # This KMS key is created by tdr-accounts which means it will exist when we run the terraform backend project for the first time
