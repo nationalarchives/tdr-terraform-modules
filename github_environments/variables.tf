493d93d38321ace1163439c84560fcd4123e094e
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-03-18 12:42:41 +0000

    Ran terraform fmt

--- github_environments/variables.tf
+++ github_environments/variables.tf
@@ -6,7 +6,7 @@ variable "team_slug" {}
 
 variable "integration_team_slug" {
   description = "This repository needs approvals on integration as well. This is the team name which can approve deployments on integration"
-  default = []
+  default     = []
 }
 
 variable "secrets" {
