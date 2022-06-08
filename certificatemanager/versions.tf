81446f293fd147265c87c380c1d12957a36e2798
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-06-08 17:38:57 +0100

    Changes for managed Grafana
    This is mostly removing the specific versions in the modules. I'm not
    sure why they're there, I'll put them back if they're needed.

--- certificatemanager/versions.tf
+++ certificatemanager/versions.tf
@@ -1,9 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.72"
-    }
-  }
-  required_version = ">= 1.1.3"
-}
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "3.72"
+#    }
+#  }
+#  required_version = ">= 1.1.3"
+#}
