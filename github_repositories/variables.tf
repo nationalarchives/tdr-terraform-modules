37302a4305b7e7503b15d4622247f670f45bbf56
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-10-05 16:54:39 +0100

    Add more detailed description to the variable

--- github_repositories/variables.tf
+++ github_repositories/variables.tf
@@ -5,7 +5,8 @@ variable "secrets" {
 }
 
 variable "collaborators" {
-  description = "Outside collaborators to add to the repository"
+
+  description = "Outside collaborators to add to the repository. This is for GitHub users who are not part of the nationalrchives organisation but who need access to a repository"
   default     = []
   type        = set(string)
 }
