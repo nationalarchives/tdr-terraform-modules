75cf058feee506c7fb575a2a18310d3bdfd3cf81
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-10-05 14:17:22 +0100

    Add outside collaborators parameter.
    This will be used to add outside collaborators to the tdr-reporting
    repo. It will read the collaborators field from this PR https://github.com/nationalarchives/tdr-configurations/pull/125

--- github_repositories/variables.tf
+++ github_repositories/variables.tf
@@ -3,3 +3,9 @@ variable "repository_name" {}
 variable "secrets" {
   default = {}
 }
+
+variable "collaborators" {
+  description = "Outside collaborators to add to the repository"
+  default     = []
+  type        = set(string)
+}
