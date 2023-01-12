8163e616d78510b2be30e3957d62e81de5608324
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-01-12 17:06:55 +0000

    Remove TDR specific templates
    The templates for the API gateway definitions and the associated
    policies are stored within terraform modules. They should be stored in
    terraform environments.
    
    Also, the aws_api_gateway_account only needs to be created once. At the
    moment, this is done by checking if the name is ExportAPI which is very
    TDR specific and not useful for tdr-terraform-modules.
    
    There's another PR pending for the changes to
    tdr-terraform-environments. This doesn't have to be a breaking change
    but it will need resources moving in the state using `terraform state mv`

---
+++ api_gateway_account/main.tf
@@ -0,0 +1,21 @@
+variable "environment" {}
+
+resource "aws_api_gateway_account" "rest_api_account" {
+  cloudwatch_role_arn = aws_iam_role.rest_api_cloudwatch_role.arn
+  depends_on          = [aws_iam_role_policy_attachment.cloudwatch_policy_attachment]
+}
+
+resource "aws_iam_role" "rest_api_cloudwatch_role" {
+  name               = "TDRApiGatewayCloudwatchRole${title(var.environment)}"
+  assume_role_policy = templatefile("${path.module}/templates/api_gateway_assume_role.json.tpl", {})
+}
+
+resource "aws_iam_policy" "rest_api_cloudwatch_policy" {
+  name   = "TDRApiGatewayCloudwatchPolicy${title(var.environment)}"
+  policy = templatefile("${path.module}/templates/api_cloudwatch_policy.json.tpl", {})
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
+  policy_arn = aws_iam_policy.rest_api_cloudwatch_policy.arn
+  role       = aws_iam_role.rest_api_cloudwatch_role.id
+}
