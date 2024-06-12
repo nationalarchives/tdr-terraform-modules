81ee5fefe74fbca5ede1c60c1fee20bd9de9a0f9
Paul Schwarzenberger <paul@celidor.net>
2024-06-12 13:30:36 +0100

    CLOP-10 Remove deprecated template provider references

--- config/main.tf
+++ config/main.tf
@@ -19,28 +19,17 @@ resource "aws_sns_topic" "config_topic" {
 
 }
 
-data "template_file" "config_assume_role_policy" {
-  template = file("./tdr-terraform-modules/config/templates/config_assume_role_policy.json.tpl")
-}
-
 resource "aws_iam_role" "config_role" {
   count              = local.region == var.primary_region ? 1 : 0
   name               = "${upper(var.project)}Config${title(local.environment)}"
-  assume_role_policy = data.template_file.config_assume_role_policy.rendered
-}
-
-data "template_file" "s3_access_policy" {
-  template = file("./tdr-terraform-modules/config/templates/s3_access_policy.json.tpl")
-  vars = {
-    bucket_name = var.bucket_id
-  }
+  assume_role_policy = templatefile("./tdr-terraform-modules/config/templates/config_assume_role_policy.json.tpl", {})
 }
 
 resource "aws_iam_policy" "s3_access_policy" {
   count       = local.region == var.primary_region ? 1 : 0
   name        = "${upper(var.project)}Config${title(local.environment)}"
   description = "Allows access to AWS Config S3 bucket"
-  policy      = data.template_file.s3_access_policy.rendered
+  policy      = templatefile("./tdr-terraform-modules/config/templates/s3_access_policy.json.tpl", {bucket_name = var.bucket_id})
 }
 
 resource "aws_iam_role_policy_attachment" "s3_policy_attach" {
@@ -49,15 +38,11 @@ resource "aws_iam_role_policy_attachment" "s3_policy_attach" {
   policy_arn = aws_iam_policy.s3_access_policy.*.arn[0]
 }
 
-data "template_file" "sns_topic_access_policy" {
-  template = file("./tdr-terraform-modules/config/templates/sns_topic_access_policy.json.tpl")
-}
-
 resource "aws_iam_policy" "sns_topic_access_policy" {
   count       = local.region == var.primary_region ? 1 : 0
   name        = "${upper(var.project)}SNSPublish${title(local.environment)}"
   description = "Allows pusblishing to SNS topic"
-  policy      = data.template_file.sns_topic_access_policy.rendered
+  policy      = templatefile("./tdr-terraform-modules/config/templates/sns_topic_access_policy.json.tpl", {})
 }
 
 resource "aws_iam_role_policy_attachment" "sns_topic_policy_attach" {
--- kms/main.tf
+++ kms/main.tf
@@ -1,11 +1,10 @@
-data "template_file" "key_policy" {
-  template = templatefile("${path.module}/templates/${var.key_policy}.json.tpl", merge(var.policy_variables, { account_id = data.aws_caller_identity.current.account_id, environment = var.environment }))
-}
-
 resource "aws_kms_key" "encryption" {
   description         = "KMS key for encryption within ${var.environment} environment"
   enable_key_rotation = true
-  policy              = data.template_file.key_policy.rendered
+  policy              = templatefile("${path.module}/templates/${var.key_policy}.json.tpl", merge(var.policy_variables, {
+    account_id = data.aws_caller_identity.current.account_id,
+    environment = var.environment
+    }))
   tags = merge(
     var.common_tags,
     tomap(
