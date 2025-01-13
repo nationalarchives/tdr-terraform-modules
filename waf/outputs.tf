255679bc5b7a33dacf317b9d4eed083c486f9c2d
ian-hoyle <ian.hoyle@nationalarchives.gov.uk>
2025-01-13 09:34:02 +0000

    add new rule group to outputs

--- waf/outputs.tf
+++ waf/outputs.tf
@@ -10,3 +10,7 @@ output "blocked_ip_set_arn" {
   value = var.blocked_ips == "" ? "" : aws_wafv2_ip_set.blocked_ips[0].arn
 }
 
+output "blocked_rule_group_arn" {
+  value = aws_wafv2_rule_group.block_ips_rule_group[0].arn
+}
+
