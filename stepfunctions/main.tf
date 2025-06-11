f22bf531c4af4092cfd24efe0a76038ee8c3a3b1
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2025-06-11 17:35:39 +0100

    fix: Confused deputy in trust policy

--- stepfunctions/main.tf
+++ stepfunctions/main.tf
@@ -1,6 +1,10 @@
 locals {
   role_name = "TDR${var.step_function_name}Role${title(var.environment)}"
 }
+
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 resource "aws_sfn_state_machine" "state_machine" {
   definition = var.definition
   name       = "${upper(var.project)}${var.step_function_name}${title(var.environment)}"
@@ -11,9 +15,10 @@ resource "aws_sfn_state_machine" "state_machine" {
   }
 }
 
+
 resource "aws_iam_role" "state_machine_role" {
   name               = local.role_name
-  assume_role_policy = templatefile("${path.module}/../iam_role/templates/assume_role.json.tpl", { service = "states.amazonaws.com" })
+  assume_role_policy = data.aws_iam_policy_document.step_function_iam_trust_policy.json
   tags = merge(
     var.tags,
     tomap(
@@ -31,4 +36,32 @@ resource "aws_iam_policy" "state_machine_policy" {
 resource "aws_iam_role_policy_attachment" "state_machine_attachment" {
   policy_arn = aws_iam_policy.state_machine_policy.arn
   role       = aws_iam_role.state_machine_role.id
+}
+
+// See TDRD-845
+// See https://docs.aws.amazon.com/step-functions/latest/dg/procedure-create-iam-role.html
+// Note using aws_sfn_state_machine.state_machine.arn creates a cyclic dependancy so cannot be used
+data "aws_iam_policy_document" "step_function_iam_trust_policy" {
+  statement {
+
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["states.amazonaws.com"]
+    }
+
+    condition {
+      test     = "ArnLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:states:${data.aws_region.current.name}:${data.aws_caller_identity.current.id}:${upper(var.project)}${var.step_function_name}${title(var.environment)}:*"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = ["${data.aws_caller_identity.current.id}"]
+    }
+  }
 }
