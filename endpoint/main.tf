390ab3ed8433f36f01312dd0b678d5565dfe7065
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2025-08-20 11:25:46 +0100

    feat: Add more params for endpoint module

--- endpoint/main.tf
+++ endpoint/main.tf
@@ -1,7 +1,9 @@
 resource "aws_vpc_endpoint" "endpoint" {
-  vpc_id       = var.vpc_id
-  service_name = var.service_name
-
+  vpc_id             = var.vpc_id
+  service_name       = var.service_name
+  vpc_endpoint_type  = var.vpc_endpoint_type
+  subnet_ids         = var.subnet_ids
+  security_group_ids = var.security_group_ids
   tags = merge(
     var.common_tags,
     tomap(
