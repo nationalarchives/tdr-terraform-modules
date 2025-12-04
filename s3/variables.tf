4c9d01626f4e4673009339175fd2a385d5022edc
TomJKing <tom.king@nationalarchives.gov.uk>
2025-12-04 10:57:21 +0000

    Remove redundant code for OAI implementation

--- s3/variables.tf
+++ s3/variables.tf
@@ -126,11 +126,6 @@ variable "log_data_sns_topic_region" {
   default     = "eu-west-2"
 }
 
-variable "cloudfront_oai" {
-  description = "The origin access id from the cloudwatch distribution which is connecting to this bucket"
-  default     = ""
-}
-
 variable "lambda_arn" {
   description = "The lambda arn to send S3 event messages to"
   default     = ""
