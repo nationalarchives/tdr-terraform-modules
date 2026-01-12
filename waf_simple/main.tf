afe311c2176c75734c2a70901d0a1494dc885ee5
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-01-12 18:24:10 +0000

    chore: WIP simple_waf

--- waf_simple/main.tf
+++ waf_simple/main.tf
@@ -10,24 +10,50 @@ resource "aws_wafv2_web_acl" "simple_acl" {
   name  = "${var.project}-${var.function}-${var.environment}-simple"
   scope = "REGIONAL"
   default_action {
-    block {}
+    allow {}
   }
 
   rule {
-    name     = "whitelist-ips"
+    name     = "block_not_in_whitelist"
     priority = 10
     action {
-      allow {}
+      block {}
     }
 
     statement {
-      ip_set_reference_statement {
-        arn = aws_wafv2_ip_set.whitelist_ips.arn
+      not_statement {
+        statement {
+          ip_set_reference_statement {
+            arn = aws_wafv2_ip_set.whitelist_ips.arn
+          }
+        }
       }
     }
     visibility_config {
       cloudwatch_metrics_enabled = false
-      metric_name                = "waf-simple-whitelist-ips"
+      metric_name                = "waf-simple-block-not-in-whitelist"
+      sampled_requests_enabled   = false
+    }
+  }
+
+  rule {
+    name     = "rate_control"
+    priority = 20
+    action {
+      block {}
+    }
+
+    statement {
+      rate_based_statement {
+        aggregate_key_type    = "IP"
+        evaluation_window_sec = 60
+        limit                 = 10
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = false
+      metric_name                = "waf-simple-rate-control"
       sampled_requests_enabled   = false
     }
   }
@@ -41,11 +67,11 @@ resource "aws_wafv2_web_acl" "simple_acl" {
   tags = var.common_tags
 }
 
-# resource "aws_wafv2_web_acl_association" "association" {
-#   count        = length(var.alb_target_groups)
-#   resource_arn = var.alb_target_groups[count.index]
-#   web_acl_arn  = aws_wafv2_web_acl.acl.arn
-# }
+resource "aws_wafv2_web_acl_association" "association" {
+  count        = length(var.associated_resources)
+  resource_arn = var.associated_resources[count.index]
+  web_acl_arn  = aws_wafv2_web_acl.simple_acl.arn
+}
 
 # resource "aws_wafv2_web_acl_logging_configuration" "waf_logging" {
 #   log_destination_configs = var.log_destinations
