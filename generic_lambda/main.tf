885730deb6307895a3aee1c6f03a52af09c9a11b
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-02-17 16:40:35 +0000

    chore: Add a default log retention period

--- generic_lambda/main.tf
+++ generic_lambda/main.tf
@@ -40,8 +40,9 @@ resource "aws_lambda_function" "lambda_function" {
 }
 
 resource "aws_cloudwatch_log_group" "lambda_log_group" {
-  name = "/aws/lambda/${aws_lambda_function.lambda_function.*.function_name[0]}"
-  tags = var.tags
+  name              = "/aws/lambda/${aws_lambda_function.lambda_function.*.function_name[0]}"
+  retention_in_days = var.cloudwatch_log_retention_in_days
+  tags              = var.tags
 }
 
 locals {
