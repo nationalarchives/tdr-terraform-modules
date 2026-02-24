ef9d999e4b7a56e44f453c4dbd2000e6d6ff5773
Ben Sampson <ben.sampson@nationalarchives.gov.uk>
2026-02-24 16:57:08 +0000

    chore: Enable info and error logging

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -19,3 +19,13 @@ resource "aws_api_gateway_stage" "api_stage" {
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
   stage_name    = var.environment
 }
+
+resource "aws_api_gateway_method_settings" "all" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  stage_name  = aws_api_gateway_stage.api_stage.stage_name
+  method_path = "*/*"
+
+  settings {
+    logging_level = "INFO"
+  }
+}
